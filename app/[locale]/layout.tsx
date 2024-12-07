2d03851dc839f63ab93cd6df56460b51d3a75089
N19099 <hdu80@interparktriple.com>
2024-12-07 14:18:05 +0900

    기본 페이지 및 i18n 추가

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,35 @@
+import Navbar from "@/components/Navbar";
+import "../globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const { locale } = await params;
+
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <div className="mx-auto max-w-4xl h-screen">
+            <Navbar locale={locale} />
+            {children}
+          </div>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
