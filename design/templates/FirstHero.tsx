066cd83de055ee237bfbb5d84ab04c1f5551bdb2
N19099 <hdu80@interparktriple.com>
2024-12-23 20:12:53 +0900

    번역 수정

--- design/templates/FirstHero.tsx
+++ design/templates/FirstHero.tsx
@@ -3,20 +3,21 @@ import { Section, HeroLeftImage } from '@/design/features/landing';
 import { buttonVariants, ReviewCard } from '@/design/components/ui';
 
 export const FirstHero = () => {
+    const t = useTranslations('FirstHero');
     return (
         <Section>
             <HeroLeftImage
                 imgSrc="/images/landing/first-hero-image.png"
                 banner={(<></>)}
-                title="nextjs boilerplate 소개 및 판매"
-                description="nextjs boilerplate를 판매합니다. 이 프로젝트가 가진 기능을 소개하고 어떻게 설치하는지 그리고 사용하는 방법도 설명해주었으면 합니다."
+                title={t('title')}
+                description={t('description')}
                 buttons={(
                     <>
                         <a
                             className={buttonVariants({ size: 'lg' })}
                             href={`/signin`}
                         >
-                            {'무료 평가판 시작하기'}
+                            {t('get_start')}
                         </a>
                     </>
                 )}
@@ -24,9 +25,9 @@ export const FirstHero = () => {
                     <>
                         <ReviewCard
                             img="/images/avatars/avatar_2.jpg"
-                            name="에밀리 존슨"
-                            desc="(크리에이티브 솔루션즈 프로젝트 매니저)"
-                            body="nextjs boilerplate는 혁신적입니다! 우리의 데이터 프로세스를 신속하게 간소화했습니다!"
+                            name={t('reviewer_name')}
+                            desc={t('reviewer_position')}
+                            body={t('reviewer_opinion')}
                         />
                     </>
                 )}
--- design/templates/SecondHero.tsx
+++ design/templates/SecondHero.tsx
@@ -3,20 +3,21 @@ import { Section, HeroRightImage } from '@/design/features/landing';
 import { buttonVariants, ReviewCard } from '@/design/components/ui';
 
 export const SecondHero = () => {
+    const t = useTranslations('SecondHero');
     return (
         <Section>
             <HeroRightImage
                 imgSrc="/images/landing/second-hero-image.png"
                 banner={(<></>)}
-                title="nextjs boilerplate 소개 및 판매"
-                description="nextjs boilerplate를 판매합니다. 이 프로젝트가 가진 기능을 소개하고 어떻게 설치하는지 그리고 사용하는 방법도 설명해주었으면 합니다."
+                title={t('title')}
+                description={t('description')}
                 buttons={(
                     <>
                         <a
                             className={buttonVariants({ size: 'lg' })}
                             href={`/signin`}
                         >
-                            {'무료 평가판 시작하기'}
+                            {t('get_start')}
                         </a>
                     </>
                 )}
@@ -24,9 +25,9 @@ export const SecondHero = () => {
                     <>
                         <ReviewCard
                             img="/images/avatars/avatar_1.jpg"
-                            name="마이클 브라운"
-                            desc="(기술 솔루션 운영 이사)"
-                            body="nextjs boilerplate 덕분에 우리의 개발 과정이 혁신적으로 바뀌었습니다 - 매끄럽고, 안전하며, 간단합니다!"
+                            name={t('reviewer_name')}
+                            desc={t('reviewer_position')}
+                            body={t('reviewer_opinion')}
                         />
                     </>
                 )}
