06c83bcbc8be7743281515b294e0898981858e5f
N19099 <hdu80@interparktriple.com>
2024-12-31 12:51:50 +0900

    테마 제공 기능 추가

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "LaunchX",
+  description: "We are providing an easier and faster way to get started with SaaS applications.",
+  url: "",
+  ogImage: "",
+  links: {
+    github: "",
+  },
+};
