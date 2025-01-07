027afef56985984934927e5ae669093d51fc1fe8
N19099 <hdu80@interparktriple.com>
2025-01-07 20:44:13 +0900

    mdx 파일 수정 진행 중

--- app/[locale]/(docs)/docs/[...slug]/page.tsx
+++ app/[locale]/(docs)/docs/[...slug]/page.tsx
@@ -3,6 +3,9 @@ import { MDXRemote } from 'next-mdx-remote/rsc'
 import matter from 'gray-matter';
 import { MdxDoc } from "@/types";
 import path from 'path';
+import { Card } from "@/design/components/ui";
+
+const components = { Card }
 
 export default async function DocumentsPage({
   params,
@@ -17,7 +20,7 @@ export default async function DocumentsPage({
 
   return (
         <div className="relative py-6 lg:gap-10 lg:py-10 prose lg:prose-xl">
-          <MDXRemote source={content} />
+          <MDXRemote source={content} components={components} />
         </div>
   );
 }
--- app/[locale]/(docs)/docs/page.tsx
+++ app/[locale]/(docs)/docs/page.tsx
@@ -2,16 +2,29 @@ import { Suspense } from "react";
 import { MdxDoc } from "@/types";
 import matter from "gray-matter";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { Card } from "@/design/components/ui";
+
+const components = { Card }
+
+interface frontMatter {
+  title: string;
+  shortTitle: string;
+  description: string;
+}
 
 export default async function DocumentsPage() {
 
   const mdx : MdxDoc = await fetch(`http://localhost:3000/api/docs/index`).then((res) => res.json());
   const { content, data } = matter(mdx.content);
+  const frontmatter = data as frontMatter;
 
   return (
         <div className="relative py-6 lg:gap-10 lg:py-10 prose lg:prose-xl">
           <Suspense fallback={<>Loading...</>}>
-            <MDXRemote source={content} />
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.shortTitle}</h2>
+          <h3>{frontmatter.description}</h3>
+            <MDXRemote source={content} components={components}/>
           </Suspense>
         </div>
   );
