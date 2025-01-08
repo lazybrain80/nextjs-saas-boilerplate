758c6dbf7db1babca8c41a7f23598908d667b218
N19099 <hdu80@interparktriple.com>
2025-01-08 20:20:00 +0900

    mdx 기능 업데이트 : 계속 진행 중

--- app/[locale]/(docs)/docs/page.tsx
+++ app/[locale]/(docs)/docs/page.tsx
@@ -19,13 +19,18 @@ export default async function DocumentsPage() {
   const frontmatter = data as frontMatter;
 
   return (
+    <Suspense fallback={<>Loading...</>}>
         <div className="relative py-6 lg:gap-10 lg:py-10 prose lg:prose-xl">
-          <Suspense fallback={<>Loading...</>}>
-          <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.shortTitle}</h2>
-          <h3>{frontmatter.description}</h3>
+          <div className="space-y-4">
+            <h1 className="font-heading inline-block text-4xl lg:text-5xl">{frontmatter.title}</h1>
+            <h2>{frontmatter.shortTitle}</h2>
+            <p className="text-xl text-muted-foreground">{frontmatter.description}</p>
+          </div>
+          <hr className="my-4"/>
+          <div className="mdx">
             <MDXRemote source={content} components={components}/>
-          </Suspense>
+          </div>
         </div>
+        </Suspense>
   );
 }
