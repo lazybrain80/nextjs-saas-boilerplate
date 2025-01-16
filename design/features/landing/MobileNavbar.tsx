9fa8a6c5ced56445311c47a0cc5fbf32a48f1029
lazybrain80 <lazybrain80@gmail.com>
2025-01-16 18:00:52 +0900

    모바일 뷰 일 경우 에러 수정

--- design/features/landing/MobileNavbar.tsx
+++ design/features/landing/MobileNavbar.tsx
@@ -22,9 +22,7 @@ export function MobileNavbar({ items, children, menuItemClick }: MobileNavProps)
         )}
         >
         <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
-            <Link href="/" className="flex items-center space-x-2">
-                <Logo />
-            </Link>
+            <Logo />
             <nav className="grid grid-flow-row auto-rows-max text-sm">
             {items.map((item, index) => (
                 <Link
