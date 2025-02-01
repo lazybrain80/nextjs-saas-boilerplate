b9b92bb820a1e9dd4f3481816cf6758cf3766795
lazybrain80 <lazybrain80@gmail.com>
2025-02-01 09:36:57 +0900

    UserAccountNav 디자인 변경

--- design/features/user-menu/user-account-nav.tsx
+++ design/features/user-menu/user-account-nav.tsx
@@ -5,12 +5,14 @@ import { useTranslations, useLocale } from 'next-intl'
 import { useRouter } from 'next/navigation'
 import { useAuthClient } from '@/auth/provider'
 import {
+  Button,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/design/components/ui'
+import * as Icons from '@/design/icons'
 import { UserAvatar } from '@/design/components'
 import { UserMetaData } from '@/types'
 
@@ -43,30 +45,61 @@ export function UserAccountNav() {
           />
         )}
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+      <DropdownMenuContent align='end' className='rounded-2xl p-4'>
         <div className='flex items-center justify-start gap-2 p-2'>
           <div className='flex flex-col space-y-1 leading-none'>
-            {user?.user_name && <p className='font-medium'>{user.user_name}</p>}
-            {user?.email && (
-              <p className='w-[200px] truncate text-sm text-muted-foreground'>
-                {user.email}
-              </p>
-            )}
+            <span className='my-2 font-bold text-xl'>User Profile</span>
+            <div className='flex items-center'>
+              <div>
+                {user && (
+                  <UserAvatar
+                    user={user}
+                    className='mx-2 h-24 w-24'
+                  />
+                )}
+              </div>
+              <div>
+                {user?.user_name && <p className='my-2 font-medium'>{user.user_name}</p>}
+                {user?.email && (
+                  <p className='w-[200px] truncate text-sm text-muted-foreground'>
+                    {user.email}
+                  </p>
+                )}
+              </div>
+            </div>
           </div>
         </div>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Link href={`/${locale}/dashboard`}>{t('dashboard')}</Link>
+        <DropdownMenuSeparator className='m-4'/>
+        <DropdownMenuItem
+          asChild
+          className='cursor-pointer'
+        >
+          <div className='flex'>
+            <div
+              className='flex items-center rounded-xl bg-blue-100 size-10 mr-4'
+            >
+              <Icons.User
+                className='m-auto text-sky-500'
+              />
+            </div>
+            <Link
+              className='text-lg hover:text-sky-500'
+              href={`/${locale}/profile`}
+            >
+              {t('profile')}
+            </Link>
+          </div>
         </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href={`/${locale}/profile`}>{t('profile')}</Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator className='m-4'/>
         <DropdownMenuItem
           className='cursor-pointer'
           onSelect={signOut}
         >
-          {t('sign_out')}
+          <Button
+            className='m-auto bg-blue-500 w-72 hover:bg-blue-700 rounded-3xl'
+          >
+            {t('sign_out')}
+          </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
