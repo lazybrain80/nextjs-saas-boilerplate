87b098e610458abe945c1c0febbe887b285a26c7
lazybrain80 <lazybrain80@gmail.com>
2025-03-02 13:35:24 +0900

    ecommerce: split 'orderby' functionality into separate component

--- design/features/ecommerce/filter-header.tsx
+++ design/features/ecommerce/filter-header.tsx
@@ -13,7 +13,7 @@ import {
 import { useCachedItems } from '@/hooks/client-cache'
 import { BaseEcommerceProduct } from './common'
 
-export const FilterHeader = () => {
+const FilterOrderby = () => {
   const { cachedItems, setCacheItems } = useCachedItems()
 
   const orderChange = (value: string) => {
@@ -38,22 +38,28 @@ export const FilterHeader = () => {
   }
 
   return (
+    <Select onValueChange={orderChange}>
+      <SelectTrigger>
+        <SelectValue placeholder='OrderBy...' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value='Newest'> Newest </SelectItem>
+        <SelectItem value='HighLow'> HighLow </SelectItem>
+        <SelectItem value='LowHigh'> LowHigh </SelectItem>
+        <SelectItem value='Discount'> Discount </SelectItem>
+      </SelectContent>
+    </Select>
+  )
+}
+
+export const FilterHeader = () => {
+  return (
     <Card className='rounded-2xl shadow-lg h-[8%] bg-white flex items-center space-x-4 p-4 mb-8 justify-between'>
       <div className='h-[50%] flex items-center space-x-4'>
         <div className='w-full'>Filter By:</div>
         <Separator orientation='vertical' />
         {/** Order by */}
-        <Select onValueChange={orderChange}>
-          <SelectTrigger>
-            <SelectValue placeholder='OrderBy...' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value='Newest'> Newest </SelectItem>
-            <SelectItem value='HighLow'> HighLow </SelectItem>
-            <SelectItem value='LowHigh'> LowHigh </SelectItem>
-            <SelectItem value='Discount'> Discount </SelectItem>
-          </SelectContent>
-        </Select>
+        <FilterOrderby />
         <Separator orientation='vertical' />
         <span>Category</span>
         <Separator orientation='vertical' />
