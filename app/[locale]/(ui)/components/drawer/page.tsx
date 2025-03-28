02f1aa6e311fdc39f5447298292ca8c9e58035c1
lazybrain80 <lazybrain80@gmail.com>
2025-03-28 20:54:20 +0900

    components: add drawer example

--- app/[locale]/(ui)/components/drawer/page.tsx
+++ app/[locale]/(ui)/components/drawer/page.tsx
@@ -1,20 +1,18 @@
 'use client'
 
 import React from 'react'
-import {
-  BoardHeader,
-} from '@/design/features/user-board'
+import { BoardHeader } from '@/design/features/user-board'
+import { DrawerExamples } from './simple-drawer'
 
-const AlertPage = () => {
-
+const DrawerExamplesPage = () => {
   return (
-    <div className='flex-1 overflow-auto bg-slate-100 p-8 space-y-6'>
+    <div className="flex-1 overflow-auto bg-slate-100 p-8 space-y-6">
       {/* Page Header */}
       <BoardHeader title={'Drawer'} />
       {/* Drawer examples */}
-
+      <DrawerExamples />
     </div>
   )
 }
 
-export default AlertPage
+export default DrawerExamplesPage
---
+++ app/[locale]/(ui)/components/drawer/simple-drawer.tsx
@@ -0,0 +1,100 @@
+'use client'
+
+import { useState } from 'react'
+import { cn } from '@/utils/cn'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  Button,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  Drawer,
+} from '@/design/components'
+import * as Icons from '@/design/icons'
+
+const SampleCodeDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Icons.Code className="w-5 h-5" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-4xl">
+        <DialogHeader>
+          <DialogTitle>Sample Code</DialogTitle>
+          <pre className="bg-slate-100 p-4 rounded-lg">
+            {`
+import { Drawer } from '@/design/components'
+
+<Drawer open={showDrawer} side={side} closeAction={closeDrawer}>
+  <div>Drawer</div>
+</Drawer>
+`}
+          </pre>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+interface DrawerProps {
+  side: 'left' | 'right' | 'top' | 'bottom'
+}
+
+const ConfigurableDrawer = ({ side }: DrawerProps) => {
+  const [showDrawer, setShowDrawer] = useState<boolean>(false)
+  const closeDrawer = () => {
+    setShowDrawer(false)
+  }
+
+  const buttonColor = {
+    left: 'bg-blue-500',
+    right: 'bg-green-500',
+    top: 'bg-yellow-500',
+    bottom: 'bg-red-500',
+  }[side]
+
+  return (
+    <>
+      <Button
+        aria-label={`Open ${side} drawer`}
+        className={cn('h-18 w-18 rounded-full text-white shadow-lg', buttonColor)}
+        onClick={() => setShowDrawer(true)}
+      >
+        Open - {side.charAt(0).toUpperCase() + side.slice(1)} Drawer
+      </Button>
+      <Drawer open={showDrawer} side={side} closeAction={closeDrawer}>
+        <div>{side.charAt(0).toUpperCase() + side.slice(1)} Drawer</div>
+      </Drawer>
+    </>
+  )
+}
+
+export const DrawerExamples = () => {
+  return (
+    <Card className="rounded-2xl bg-white">
+      <CardHeader>
+        <CardTitle>
+          <div className="flex items-center justify-between">
+            <div> Drawer Examples </div>
+            <SampleCodeDialog />
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex items-center justify-center space-x-4">
+          <ConfigurableDrawer side="left" />
+          <ConfigurableDrawer side="right" />
+          <ConfigurableDrawer side="top" />
+          <ConfigurableDrawer side="bottom" />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
