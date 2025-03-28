d2401a79c67e93a8bcf042b571b86d67fc686bfa
lazybrain80 <lazybrain80@gmail.com>
2025-03-28 18:14:30 +0900

    components: add sample codes for component examples

---
+++ app/[locale]/(ui)/components/text-editor/simple-dialog.tsx
@@ -0,0 +1,59 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  Button,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/design/components'
+import * as Icons from '@/design/icons'
+
+const SampleCodeDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Icons.Code className="w-5 h-5" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-4xl">
+        <DialogHeader>
+          <DialogTitle>Sample Code</DialogTitle>
+          <pre className="bg-slate-100 p-4 rounded-lg">
+            {`
+import { ChipsInput } from '@/design/components'
+
+<ChipsInput
+  placeholder="Add a chip..."
+  initialChips={['Example Chip 1', 'Example Chip 2']}
+  showFooter
+/>
+`}
+          </pre>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export const SimpleDialog = () => {
+  return (
+    <Card className="rounded-2xl bg-white">
+      <CardHeader>
+        <CardTitle>
+          <div className="flex items-center justify-between">
+            <div> Simple Chip Input </div>
+            <SampleCodeDialog />
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex items-center justify-center space-x-4">{/** component sample */}</div>
+      </CardContent>
+    </Card>
+  )
+}
