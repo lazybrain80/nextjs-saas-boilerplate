b4aef9f0342857016c4c9c435ee84d520445d17a
lazybrain80 <lazybrain80@gmail.com>
2025-04-01 20:09:42 +0900

    components: add progress example

--- app/[locale]/(ui)/components/progress/page.tsx
+++ app/[locale]/(ui)/components/progress/page.tsx
@@ -1,20 +1,18 @@
 'use client'
 
 import React from 'react'
-import {
-  BoardHeader,
-} from '@/design/features/user-board'
+import { BoardHeader } from '@/design/features/user-board'
+import { SimpleProgress } from './simple-progress'
 
-const AlertPage = () => {
-
+const ProgressExamplePage = () => {
   return (
-    <div className='flex-1 overflow-auto bg-slate-100 p-8 space-y-6'>
+    <div className="flex-1 overflow-auto bg-slate-100 p-8 space-y-6">
       {/* Page Header */}
       <BoardHeader title={'Progress'} />
       {/* Progress examples */}
-
+      <SimpleProgress />
     </div>
   )
 }
 
-export default AlertPage
+export default ProgressExamplePage
