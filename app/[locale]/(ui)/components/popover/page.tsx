e7d496dac56c78c51f543f846b08a6bdd45a9cb4
lazybrain80 <lazybrain80@gmail.com>
2025-04-01 19:45:58 +0900

    components: add popover example

--- app/[locale]/(ui)/components/popover/page.tsx
+++ app/[locale]/(ui)/components/popover/page.tsx
@@ -1,20 +1,18 @@
 'use client'
 
 import React from 'react'
-import {
-  BoardHeader,
-} from '@/design/features/user-board'
+import { BoardHeader } from '@/design/features/user-board'
+import { SimplePopover } from './simple-popover'
 
-const AlertPage = () => {
-
+const PopoverExamplePage = () => {
   return (
-    <div className='flex-1 overflow-auto bg-slate-100 p-8 space-y-6'>
+    <div className="flex-1 overflow-auto bg-slate-100 p-8 space-y-6">
       {/* Page Header */}
       <BoardHeader title={'Popover'} />
       {/* Popover examples */}
-
+      <SimplePopover />
     </div>
   )
 }
 
-export default AlertPage
+export default PopoverExamplePage
