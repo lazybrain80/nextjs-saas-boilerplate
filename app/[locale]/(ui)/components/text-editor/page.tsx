20ca936f0882e8879095a8ee6cbd70757e453a17
lazybrain80 <lazybrain80@gmail.com>
2025-04-04 20:40:52 +0900

    components: add text editor example

--- app/[locale]/(ui)/components/text-editor/page.tsx
+++ app/[locale]/(ui)/components/text-editor/page.tsx
@@ -1,20 +1,17 @@
 'use client'
 
 import React from 'react'
-import {
-  BoardHeader,
-} from '@/design/features/user-board'
-
-const AlertPage = () => {
-
+import { BoardHeader } from '@/design/features/user-board'
+import { SimpleTextEditor } from './simple-text-editor'
+const TextEditorExamplePage = () => {
   return (
-    <div className='flex-1 overflow-auto bg-slate-100 p-8 space-y-6'>
+    <div className="flex-1 overflow-auto bg-slate-100 p-8 space-y-6">
       {/* Page Header */}
       <BoardHeader title={'Text Editor'} />
       {/* Text Editor examples */}
-
+      <SimpleTextEditor />
     </div>
   )
 }
 
-export default AlertPage
+export default TextEditorExamplePage
--- app/[locale]/(ui)/components/text-editor/simple-text-editor.tsx
+++ app/[locale]/(ui)/components/text-editor/simple-text-editor.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState } from 'react'
 import {
   Card,
   CardHeader,
@@ -9,7 +12,9 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  TextEditor,
 } from '@/design/components'
+import { Editor } from '@tiptap/react'
 import * as Icons from '@/design/icons'
 
 const SampleCodeDialog = () => {
@@ -25,12 +30,11 @@ const SampleCodeDialog = () => {
           <DialogTitle>Sample Code</DialogTitle>
           <pre className="bg-slate-100 p-4 rounded-lg">
             {`
-import { ChipsInput } from '@/design/components'
+import { TextEditor } from '@/design/components'
 
-<ChipsInput
-  placeholder="Add a chip..."
-  initialChips={['Example Chip 1', 'Example Chip 2']}
-  showFooter
+<TextEditor
+  onCreate={editor => setEditor(editor)}
+  onTextCountChange={count => setTotalText(count)}
 />
 `}
           </pre>
@@ -40,19 +44,28 @@ import { ChipsInput } from '@/design/components'
   )
 }
 
-export const SimpleDialog = () => {
+export const SimpleTextEditor = () => {
+  const [editor, setEditor] = useState<Editor | null>(null)
+  const [totalText, setTotalText] = useState<number>(0)
+
   return (
     <Card className="rounded-2xl bg-white">
       <CardHeader>
         <CardTitle>
           <div className="flex items-center justify-between">
-            <div> Simple Chip Input </div>
+            <div> TextEditor example </div>
             <SampleCodeDialog />
           </div>
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex items-center justify-center space-x-4">{/** component sample */}</div>
+        <div className="flex items-center justify-center space-x-4">
+          <TextEditor
+            className="w-full"
+            onCreate={editor => setEditor(editor)}
+            onTextCountChange={count => setTotalText(count)}
+          />
+        </div>
       </CardContent>
     </Card>
   )
